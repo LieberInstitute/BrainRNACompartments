68c5c5c20ac0f03c736230abb1738814a6f6a5fc
aprice26 <amanda.joy.price@gmail.com>
2017-04-17 01:12:36 -0400

    updated figure 3 LFC code for age differences

--- RNA_localization_and_age/foldChangeByFractionAndAge.R
+++ RNA_localization_and_age/foldChangeByFractionAndAge.R
@@ -88,187 +88,90 @@ ggplot(ribo[which((ribo$Sig.A=="YES" | ribo$Sig.F=="YES") & ribo$quad!="NA"),],
   theme(title = element_text(size = 20)) +
   theme(text = element_text(size = 20))
 
-# 
-
-
-names(DirList) = c("Adult\nPolyA\nUp", "Adult\nPolyA\nDown", "Fetal\nPolyA\nUp", "Fetal\nPolyA\nDown", 
-                   "Adult\nRibozero\nUp", "Adult\nRibozero\nDown", "Fetal\nRibozero\nUp", 
-                   "Fetal\nRibozero\nDown")
-APu = DirList[["Adult\nPolyA\nUp"]]
-FPu = DirList[["Fetal\nPolyA\nUp"]]
-ARu = DirList[["Adult\nRibozero\nUp"]]
-FRu = DirList[["Fetal\nRibozero\nUp"]]
-APd = DirList[["Adult\nPolyA\nDown"]]
-FPd = DirList[["Fetal\nPolyA\nDown"]]
-ARd = DirList[["Adult\nRibozero\nDown"]]
-FRd = DirList[["Fetal\nRibozero\nDown"]]
-upP = unique(c(as.character(APu$X), as.character(FPu$X)))
-upR = unique(c(as.character(ARu$X), as.character(FRu$X)))
-dP = unique(c(as.character(APd$X), as.character(FPd$X)))
-dR = unique(c(as.character(APd$X), as.character(FPd$X)))
-polyA=polyA[which(polyA$quad=="same" | polyA$quad=="diff"),]
-ribo=ribo[which(ribo$quad=="same" | ribo$quad=="diff"),]
-venn.polyA.UP = list("Different Direction" = polyA[which(polyA$quad=="diff" & polyA$Gene %in% upP),],
-                     "Same Direction" = polyA[which(polyA$quad=="same" & polyA$Gene %in% upP),],
-                  "Adult:Nucleus"=DirList[["Adult\nPolyA\nUp"]], "Fetal:Nucleus"=DirList[["Fetal\nPolyA\nUp"]])
-venn.polyA.D = list("Different Direction"=polyA[which(polyA$quad=="diff" & polyA$Gene %in% dP),], 
-                    "Same Direction"=polyA[which(polyA$quad=="same" & polyA$Gene %in% dP),],
-                    "Adult:Cytosol"=DirList[["Adult\nPolyA\nDown"]], "Fetal:Cytosol"=DirList[["Fetal\nPolyA\nDown"]])
-venn.polyA.UP = lapply(venn.polyA.UP, function(x) as.character(x[,1]))
-venn.polyA.D = lapply(venn.polyA.D, function(x) as.character(x[,1]))
-venn.ribo.UP = list("Different Direction" = ribo[which(ribo$quad=="diff" & ribo$Gene %in% upR),],
-                     "Same Direction" = ribo[which(ribo$quad=="same" & ribo$Gene %in% upR),],
-                     "Adult:Nucleus"=DirList[["Adult\nRibozero\nUp"]], "Fetal:Nucleus"=DirList[["Fetal\nRibozero\nUp"]])
-venn.ribo.D = list("Different Direction"=ribo[which(ribo$quad=="diff" & ribo$Gene %in% dR),], 
-                    "Same Direction"=ribo[which(ribo$quad=="same" & ribo$Gene %in% dR),],
-                    "Adult:Cytosol"=DirList[["Adult\nRibozero\nDown"]], "Fetal:Cytosol"=DirList[["Fetal\nRibozero\nDown"]])
-venn.ribo.UP = lapply(venn.ribo.UP, function(x) as.character(x[,1]))
-venn.ribo.D = lapply(venn.ribo.D, function(x) as.character(x[,1]))
-
-venn.p.UP <- venn.diagram(venn.polyA.UP, "/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/Figure_PDFs/venn.polyA_UP_LFC_and_DEGs.jpeg", 
-                          main="Overlap of Nuclear DEGs and LCF Direction (polyA)",
-                          col = "transparent",
-                          fill = c("lightpink2","cornflowerblue", "olivedrab2", "darkorchid1"),
-                          alpha = 0.50,
-                          label.col = c("olivedrab4", "white", "darkorchid4", "white",
-                                        "white", "white", "white", "white", "palevioletred4", "white",
-                                        "white", "white", "white", "darkblue", "white"),
-                          fontfamily = "Arial",
-                          fontface = "bold",
-                          cat.col = c("palevioletred4", "darkblue", "olivedrab4", "darkorchid4"),
-                          cat.fontfamily = "Arial", margin=0.2, cat.dist=0.3)
-venn.p.D <- venn.diagram(venn.polyA.D, "/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/Figure_PDFs/venn.polyA_D_LFC_and_DEGs.jpeg", 
-                         main="Overlap of Cytosolic DEGs and LCF Direction (polyA)",
-                         col = "transparent",
-                         fill = c("lightpink2","cornflowerblue", "olivedrab2", "darkorchid1"),
-                         alpha = 0.50,
-                         label.col = c("olivedrab4", "white", "darkorchid4", "white",
-                                       "white", "white", "white", "white", "palevioletred4", "white",
-                                       "white", "white", "white", "darkblue", "white"),
-                         fontfamily = "Arial",
-                         fontface = "bold",
-                         cat.col = c("palevioletred4", "darkblue", "olivedrab4", "darkorchid4"),
-                         cat.fontfamily = "Arial", margin=0.2, cat.dist=0.3)
-
-
-venn.r.UP <- venn.diagram(venn.ribo.UP, "/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/Figure_PDFs/venn.ribo_UP_LFC_and_DEGs.jpeg", 
-                           main="Overlap of Nuclear DEGs and LCF Direction (Ribozero)",
-                           col = "transparent",
-                           fill = c("lightpink2","cornflowerblue", "olivedrab2", "darkorchid1"),
-                           alpha = 0.50,
-                           label.col = c("olivedrab4", "white", "darkorchid4", "white",
-                                         "white", "white", "white", "white", "palevioletred4", "white",
-                                         "white", "white", "white", "darkblue", "white"),
-                           fontfamily = "Arial",
-                           fontface = "bold",
-                           cat.col = c("palevioletred4", "darkblue", "olivedrab4", "darkorchid4"),
-                           cat.fontfamily = "Arial", margin=0.2, cat.dist=0.3)
-venn.r.D <- venn.diagram(venn.ribo.D, "/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/Figure_PDFs/venn.ribo_D_LFC_and_DEGs.jpeg", 
-                          main="Overlap of Cytosolic DEGs and LCF Direction (Ribozero)",
-                          col = "transparent",
-                          fill = c("lightpink2","cornflowerblue", "olivedrab2", "darkorchid1"),
-                          alpha = 0.50,
-                          label.col = c("olivedrab4", "white", "darkorchid4", "white",
-                                        "white", "white", "white", "white", "palevioletred4", "white",
-                                        "white", "white", "white", "darkblue", "white"),
-                          fontfamily = "Arial",
-                          fontface = "bold",
-                          cat.col = c("palevioletred4", "darkblue", "olivedrab4", "darkorchid4"),
-                          cat.fontfamily = "Arial", margin=0.2, cat.dist=0.3)
-
-
 ### By Age
 # Prepare significant genes in a list
-AgeList = list(Cpres = read.csv("/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/RDAs/Cpres.csv"),
-                Npres = read.csv("/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/RDAs/Npres.csv"),
-                Crres = read.csv("/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/RDAs/Crres.csv"),
-                Nrres = read.csv("/Users/amanda/Dropbox/NucVsCytosol/Manuscript_Materials/RDAs/Nrres.csv"))
-
-SigAgeList = lapply(AgeList, function(x) x[which(x$padj<=0.05),])
-elementLengths(SigAgeList)
-CP = SigAgeList[[1]]
-NP = SigAgeList[[2]]
-CR = SigAgeList[[3]]
-NR = SigAgeList[[4]]
-sigGenesP = unique(c(as.character(CP[,1]),as.character(NP[,1])))
-sigGenesR = unique(c(as.character(CR[,1]),as.character(NR[,1])))
-SigP = lapply(AgeList[1:2], function(x) x[which(x$X %in% sigGenesP),])
-SigR = lapply(AgeList[3:4], function(x) x[which(x$X %in% sigGenesR),])
+AgeList = list(Cpres = data.frame(Cpres), Npres = data.frame(Npres), 
+                Crres = data.frame(Crres), Nrres = data.frame(Nrres),
+                Cpres_down = data.frame(Cpres.down))
+DirList = Map(cbind, AgeList, 
+              GeneID = lapply(AgeList, function(x) rownames(x)),
+              Sign = lapply(AgeList, function(x) ifelse(x$log2FoldChange > 0,"UpPrenatal", "DownPrenatal")),
+              Comparison = list("Cytosol:PolyA","Nucleus:PolyA",
+                                "Cytosol:RiboZero","Nucleus:RiboZero","Nucleus:PolyA"),
+              Sig=lapply(AgeList, function(x) ifelse(x$padj<=0.05,"YES", "NO")))
 
 # in all genes
-x = lapply(AgeList, function(x) x[order(x$X),])
-lfc = list()
-for (i in 1:length(AgeList)){
-  tmp = x[[i]]
-  lfc[[i]] = data.frame(Gene=tmp$X, baseMean = tmp$baseMean, log2FoldChange = tmp$log2FoldChange, padj = tmp$padj, comparison = names[i])
-  tmp2 = lfc[[i]]
-  tmp2$Sig= ifelse(tmp2$padj<=0.05,"YES", "NO")
-  lfc[[i]] = tmp2
-}
-CP = lfc[[1]]
-NP = lfc[[2]]
-CR = lfc[[3]]
-NR = lfc[[4]]
+DirList = lapply(DirList, function(x) x[order(x$GeneID),])
+polya = data.frame(gene.C = DirList[["Cpres"]][,7], gene.F = DirList[["Npres"]][,7],
+                   LFC.C = DirList[["Cpres"]][,2], LFC.N = DirList[["Npres"]][,2],
+                   Sign.C = DirList[["Cpres"]][,8], Sign.N = DirList[["Npres"]][,8],
+                   Sig.C = DirList[["Cpres"]][,10], Sig.N = DirList[["Npres"]][,10])
+ribo = data.frame(gene.C = DirList[["Crres"]][,7], gene.N = DirList[["Nrres"]][,7],
+                  LFC.C = DirList[["Crres"]][,2], LFC.N = DirList[["Nrres"]][,2],
+                  Sign.C = DirList[["Crres"]][,8], Sign.N = DirList[["Nrres"]][,8],
+                  Sig.C = DirList[["Crres"]][,10], Sig.N = DirList[["Nrres"]][,10])
+x = DirList[["Npres"]]
+x = x[match(as.character(DirList[["Cpres_down"]][,7]),as.character(x$GeneID)),]
+polya.down = data.frame(gene.N = x[,7], gene.C = DirList[["Cpres_down"]][,7],
+                        LFC.N = x[,2], LFC.C = DirList[["Cpres_down"]][,2],
+                        Sign.N = x[,8], Sign.C = DirList[["Cpres_down"]][,8],
+                        Sig.N = x[,10], Sig.C = DirList[["Cpres_down"]][,10])
+identical(as.character(polya.down$gene.C),as.character(polya.down$gene.N))
+x = ifelse(as.character(polya.down$gene.C)==as.character(polya.down$gene.N), "YES","NO")
+x[which(x=="NO")]
+identical(as.character(polya$gene.C),as.character(polya$gene.N))
+identical(as.character(ribo$gene.C),as.character(ribo$gene.N))
+polya$quad = ifelse(polya$Sign.C==polya$Sign.N, "same","diff")
+polya.down$quad = ifelse(polya.down$Sign.C==polya.down$Sign.N, "same","diff")
+ribo$quad = ifelse(ribo$Sign.C==ribo$Sign.N, "same","diff")
 
-polyA = data.frame(Gene = CP$Gene, Adult.LFC = CP$log2FoldChange, Fetal.LFC = NP$log2FoldChange)
-ribo = data.frame(Gene = CR$Gene, Adult.LFC = CR$log2FoldChange, Fetal.LFC = NR$log2FoldChange)
-polyA$Cs=ifelse(polyA$Adult.LFC>0, "pos","neg")
-polyA$Ns=ifelse(polyA$Fetal.LFC>0, "pos","neg")
-polyA$quad=ifelse(polyA$Cs==polyA$Ns, "same","diff")
-ribo$Cs=ifelse(ribo$Adult.LFC>0, "pos","neg")
-ribo$Ns=ifelse(ribo$Fetal.LFC>0, "pos","neg")
-ribo$quad=ifelse(ribo$Cs==ribo$Ns, "same","diff")
-
-ggplot(polyA, aes(Adult.LFC, Fetal.LFC)) + geom_point(aes(colour = factor(quad))) +
+ggplot(polya[which(polya$quad!="NA"),],
+       aes(LFC.C, LFC.N)) + geom_point(aes(colour = factor(quad))) +
   ylab("Nuclear") + 
   xlab("Cytosolic") +
   ylim(-8,8) + xlim(-8,8) +
   ggtitle("Log2 Fold Change\nBetween Ages (PolyA)") + 
   theme(title = element_text(size = 20)) +
   theme(text = element_text(size = 20))
-ggplot(ribo, aes(Adult.LFC, Fetal.LFC)) + geom_point(aes(colour = factor(quad))) +
+ggplot(ribo[which(ribo$quad!="NA"),],
+       aes(LFC.C, LFC.N)) + geom_point(aes(colour = factor(quad))) +
   ylab("Nuclear") + 
   xlab("Cytosolic") +
   ylim(-8,8) + xlim(-8,8) +
   ggtitle("Log2 Fold Change\nBetween Ages (RiboZero)") + 
+  theme(title = element_text(size = 20)) +
+  theme(text = element_text(size = 20))
+ggplot(polya.down[which(polya.down$quad!="NA"),],
+       aes(LFC.C, LFC.N)) + geom_point(aes(colour = factor(quad))) +
+  ylab("Nuclear") + 
+  xlab("Cytosolic") +
+  ylim(-8,8) + xlim(-8,8) +
+  ggtitle("Log2 Fold Change\nBetween Ages (PolyA)") + 
   theme(title = element_text(size = 20)) +
   theme(text = element_text(size = 20))
 
 # in significant genes
-SigP = lapply(SigP, function(x) x[order(x$X),])
-SigR = lapply(SigR, function(x) x[order(x$X),])
-lfcP = lfcR = list()
-for (i in 1:length(SigP)){
-  tmpP = SigP[[i]]
-  tmpR = SigR[[i]]
-  lfcP[[i]] = data.frame(Gene=tmpP$X, baseMean = tmpP$baseMean, log2FoldChange = tmpP$log2FoldChange, padj = tmpP$padj)
-  lfcR[[i]] = data.frame(Gene=tmpR$X, baseMean = tmpR$baseMean, log2FoldChange = tmpR$log2FoldChange, padj = tmpR$padj)
-}
-CP = lfcP[[1]]
-NP = lfcP[[2]]
-CR = lfcR[[1]]
-NR = lfcR[[2]]
-
-polyA = data.frame(Gene = CP$Gene, Adult.LFC = CP$log2FoldChange, Fetal.LFC = NP$log2FoldChange)
-ribo = data.frame(Gene = CR$Gene, Adult.LFC = CR$log2FoldChange, Fetal.LFC = NR$log2FoldChange)
-polyA$Cs=ifelse(polyA$Adult.LFC>0, "pos","neg")
-polyA$Ns=ifelse(polyA$Fetal.LFC>0, "pos","neg")
-polyA$quad=ifelse(polyA$Cs==polyA$Ns, "same","diff")
-ribo$Cs=ifelse(ribo$Adult.LFC>0, "pos","neg")
-ribo$Ns=ifelse(ribo$Fetal.LFC>0, "pos","neg")
-ribo$quad=ifelse(ribo$Cs==ribo$Ns, "same","diff")
-
-ggplot(polyA, aes(Adult.LFC, Fetal.LFC)) + geom_point(aes(colour = factor(quad))) +
+ggplot(polya[which((polya$Sig.C=="YES" | polya$Sig.N=="YES") & polya$quad!="NA"),],
+       aes(LFC.C, LFC.N)) + geom_point(aes(colour = factor(quad))) +
   ylab("Nuclear") + 
   xlab("Cytosolic") +
   ylim(-8,8) + xlim(-8,8) +
   ggtitle("Log2 Fold Change\nBetween Ages (PolyA)") + 
   theme(title = element_text(size = 20)) +
   theme(text = element_text(size = 20))
-ggplot(ribo, aes(Adult.LFC, Fetal.LFC)) + geom_point(aes(colour = factor(quad))) +
+ggplot(ribo[which((ribo$Sig.C=="YES" | ribo$Sig.N=="YES") & ribo$quad!="NA"),],
+       aes(LFC.C, LFC.N)) + geom_point(aes(colour = factor(quad))) +
   ylab("Nuclear") + 
   xlab("Cytosolic") +
   ylim(-8,8) + xlim(-8,8) +
   ggtitle("Log2 Fold Change\nBetween Ages (RiboZero)") + 
+  theme(title = element_text(size = 20)) +
+  theme(text = element_text(size = 20))
+ggplot(polya.down[which((polya.down$Sig.C=="YES" | polya.down$Sig.N=="YES") & polya.down$quad!="NA"),],
+       aes(LFC.C, LFC.N)) + geom_point(aes(colour = factor(quad))) +
+  ylab("Nuclear") + 
+  xlab("Cytosolic") +
+  ylim(-8,8) + xlim(-8,8) +
+  ggtitle("Log2 Fold Change\nBetween Ages (PolyA)") + 
   theme(title = element_text(size = 20)) +
   theme(text = element_text(size = 20))
