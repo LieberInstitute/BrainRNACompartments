f3e7d84199df0d743df4e8a45261afc63d9e79d2
Amanda Price <aprice26@jhpce01.cm.cluster>
2017-05-09 19:43:56 -0400

    changed code to reflect server 4 config

--- intron_retention/split_RBPMap_results_byAsterisk.sh
+++ intron_retention/split_RBPMap_results_byAsterisk.sh
@@ -1,5 +1,8 @@
-cd /dcl01/lieber/ajaffe/Amanda/NucVsCyt/RBPMap
+# For whatever reason, this only works on server 4 (and not on the biostat cluster or my LIBD workstation)
+# in the directory /media/DATA/Amanda/RBPMap, run 'sh parser.sh'
 
-filename=/dcl01/lieber/ajaffe/Amanda/NucVsCyt/RBPMap/RBPMap_results_hg19.txt
+cd /media/DATA/Amanda/RBPMap
+
+filename=/media/DATA/Amanda/RBPMap/RBPMap_results_hg19.txt
 
 awk '/^*/{i++};{print > (i".txt")}' $filename
