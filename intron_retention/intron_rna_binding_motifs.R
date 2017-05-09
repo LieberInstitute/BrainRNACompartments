c0234795348f896a8d65699a3333ac0a9c82d211
aprice26 <amanda.joy.price@gmail.com>
2017-05-09 11:21:09 -0400

    removed code not related to rna binding motifs

--- intron_retention/intron_rna_binding_motifs.R
+++ intron_retention/intron_rna_binding_motifs.R
@@ -82,24 +82,3 @@ write.table(rnacoord[,"ID"], file="./Dropbox/sorted_figures/new/github_controlle
 
 
   # more information can be found at http://rbpmap.technion.ac.il/
-  
-
-###############################################################
-####################### intron position #######################
-###############################################################
-  
-### Check intron position in the transcript
-
-ensembl = useMart("ENSEMBL_MART_ENSEMBL", # VERSION 75, hg19
-                  dataset="hsapiens_gene_ensembl",
-                  host="feb2014.archive.ensembl.org")
-sym = getBM(attributes = c("chromosome_name", "exon_chrom_start","exon_chrom_end",
-                           "strand","ensembl_exon_id", "ensembl_transcript_id", "ensembl_gene_id"),
-            mart=ensembl)
-save(sym, file="/dcl01/lieber/ajaffe/Amanda/NucVsCyt/IRfinder_redo/biomaRt.transcript.annotation.rda")
-
-geneMap$Symbol = sym$hgnc_symbol[match(rownames(geneMap), sym$ensembl_gene_id)]
-geneMap$EntrezID = sym$entrezgene[match(rownames(geneMap), sym$ensembl_gene_id)]
-
-# Gene Ontology of genes containing differentially retained introns
-
