2f67d24e38b7b5a5ed5af291f621baa098a869bc
aprice26 <amanda.joy.price@gmail.com>
2017-09-01 18:01:51 -0400

    added density position in transcript plots

--- intron_retention/intron_position_in_transcript.R
+++ intron_retention/intron_position_in_transcript.R
@@ -73,8 +73,8 @@ Tx = lapply(Tx, function(x) x[,c(1:4, 6:16)])
 
 
 ## Check intron position in the transcript
-# calculate the distance of midpoint of intron from the start of the last exon divided by the length of the transcript
-position = lapply(Tx, function(x) (x$TxEnd-(x$start + x$width/2))/(x$TxEnd-x$FirstStart))
+# calculate the distance of midpoint of intron from the end of the transcript divided by the length of the transcript
+position = lapply(Tx, function(x) (x$TxEnd-(x$start + x$width/2))/(x$TxEnd-x$FirstStart)) # the smaller the value, the closer to the 3' end
 lapply(position, head)
 
 # summary statistics on position
@@ -84,6 +84,46 @@ stats$group = c("All", "All", "Cytosolic Retention\nIn Adult","Nuclear Retention
                 "Nuclear Retention", "Adult Retention", "Prenatal Retention")
 stats$retention = c("NA","NA","Cytosolic","Nuclear","Cytosolic","Nuclear","Adult","Prenatal","Adult","Prenatal","Nuclear","Adult", "Prenatal")
 
+
+# distribution of introns measured
+posDF = lapply(position, function(x) data.frame(position = x, Comparison = NA))
+for (i in 1:length(posDF)){posDF[[i]][,"Comparison"] = names(position)[i]}
+posDF = do.call(rbind, posDF)
+posDF = posDF[which(posDF$Comparison!="Adult-Increased" & posDF$Comparison!="Prenatal-Increased" &
+                      posDF$Comparison!="Nucleus-Increased"),]
+posDF$Comparison = factor(posDF$Comparison, levels = c("Introns (Fraction)","Introns (Age)","Adult:Cytosol-Increased",
+                                                       "Adult:Nucleus-Increased","Prenatal:Cytosol-Increased",
+                                                       "Prenatal:Nucleus-Increased","Cytosol:Adult-Increased",
+                             "Cytosol:Prenatal-Increased","Nucleus:Adult-Increased","Nucleus:Prenatal-Increased"))
+posDF$FracAge = ifelse((posDF$Comparison=="Introns (Fraction)" | posDF$Comparison=="Adult:Cytosol-Increased" |
+                          posDF$Comparison=="Adult:Nucleus-Increased" |posDF$Comparison=="Prenatal:Cytosol-Increased" |
+                          posDF$Comparison=="Prenatal:Nucleus-Increased"), "Fraction", "Age")
+pdf("./Dropbox/sorted_figures/new/github_controlled/intron_retention/figures/intron_IR_comparisons/density_proportionalDistance_fromIntron_toTxEnd_fraction.pdf", width=8.5,height=5)
+ggplot(posDF[which(posDF$FracAge=="Fraction"),], aes(x=position)) +
+    geom_density(aes(group=Comparison, colour=Comparison)) +
+    ylab("") +
+    xlim(0,1) +
+    xlab("Proportion of Transcript") +
+    ggtitle(paste0("Distance from Transcript End")) +
+    theme(title = element_text(size = 20)) +
+    theme(text = element_text(size = 20)) +
+    labs(fill="") +
+    theme(legend.background = element_rect(fill = "transparent"),
+          legend.key = element_rect(fill = "transparent", color = "transparent"))
+dev.off()
+pdf("./Dropbox/sorted_figures/new/github_controlled/intron_retention/figures/intron_IR_comparisons/density_proportionalDistance_fromIntron_toTxEnd_age.pdf", width=8.5,height=5)
+ggplot(posDF[which(posDF$FracAge=="Age"),], aes(x=position)) +
+  geom_density(aes(group=Comparison, colour=Comparison)) +
+  ylab("") +
+  xlim(0,1) +
+  xlab("Proportion of Transcript") +
+  ggtitle(paste0("Distance from Transcript End")) +
+  theme(title = element_text(size = 20)) +
+  theme(text = element_text(size = 20)) +
+  labs(fill="") +
+  theme(legend.background = element_rect(fill = "transparent"),
+        legend.key = element_rect(fill = "transparent", color = "transparent"))
+dev.off()
 
 ## Is there a difference by fraction
 t.test(x=c(position[["Adult:Cytosol-Increased"]], position[["Prenatal:Cytosol-Increased"]]), 
@@ -116,6 +156,60 @@ t.test(x=position[["Prenatal:Cytosol-Increased"]], y=position[["Prenatal:Nucleus
 #  mean of x mean of y 
 #0.8993157 0.5514879
 
+## Is there a difference between fraction-regulated and non-regulated introns
+t.test(x=c(position[["Adult:Cytosol-Increased"]], position[["Prenatal:Cytosol-Increased"]],
+           position[["Adult:Nucleus-Increased"]], position[["Prenatal:Nucleus-Increased"]]), 
+       y=position[["Introns (Fraction)"]])
+#t = 1.8337, df = 2459.6, p-value = 0.06683
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -0.001462217  0.043595072
+#sample estimates:
+#  mean of x mean of y 
+#0.4866338 0.4655673
+
+## Is there a difference between cytosol-increased and non-regulated introns
+t.test(x=c(position[["Adult:Cytosol-Increased"]], position[["Prenatal:Cytosol-Increased"]]), 
+       y=position[["Introns (Fraction)"]])
+#t = 6.0747, df = 15.517, p-value = 1.837e-05
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  0.1931304 0.4009889
+#sample estimates:
+#  mean of x mean of y 
+#0.7626270 0.4655673
+
+## Is there a difference between nucleus-increased and non-regulated introns
+t.test(x=c(position[["Adult:Nucleus-Increased"]], position[["Prenatal:Nucleus-Increased"]]), 
+       y=position[["Introns (Fraction)"]])
+#t = 1.5093, df = 2411, p-value = 0.1313
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -0.005212378  0.040052265
+#sample estimates:
+#  mean of x mean of y 
+#0.4829873 0.4655673
+
+## Is there a difference between cytosol-increased in adult and non-regulated introns
+t.test(x=position[["Adult:Cytosol-Increased"]], y=position[["Introns (Fraction)"]])
+#t = 4.2152, df = 11.253, p-value = 0.001378
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  0.1205352 0.3824583
+#sample estimates:
+#  mean of x mean of y 
+#0.7170641 0.4655673
+
+## Is there a difference between cytosol-increased in adult and non-regulated introns
+t.test(x=position[["Prenatal:Cytosol-Increased"]], y=position[["Introns (Fraction)"]])
+#t = 65.802, df = 503.3, p-value < 2.2e-16
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  0.4207977 0.4466990
+#sample estimates:
+#  mean of x mean of y 
+#0.8993157 0.4655673
+
 ## Is there a difference by age
 t.test(x=c(position[["Cytosol:Adult-Increased"]], position[["Nucleus:Adult-Increased"]]), 
        y=c(position[["Cytosol:Prenatal-Increased"]], position[["Nucleus:Prenatal-Increased"]]), alternative = "greater")
@@ -146,3 +240,14 @@ t.test(x=position[["Nucleus:Adult-Increased"]], y=position[["Nucleus:Prenatal-In
 #sample estimates:
 #  mean of x mean of y 
 #0.4916112 0.4765712
+
+t.test(x=c(position[["Cytosol:Adult-Increased"]], position[["Nucleus:Adult-Increased"]],
+           position[["Cytosol:Prenatal-Increased"]], position[["Nucleus:Prenatal-Increased"]]), 
+       y=position[["Introns (Age)"]])
+#t = 0.58368, df = 3552.1, p-value = 0.5595
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -0.01937302  0.03579730
+#sample estimates:
+#  mean of x mean of y 
+#0.4993261 0.4911140
