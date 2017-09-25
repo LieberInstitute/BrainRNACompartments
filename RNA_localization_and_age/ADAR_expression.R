062ba70681929e94005a9d8995183708eb740ef8
aprice26 <amanda.joy.price@gmail.com>
2017-09-25 17:12:13 -0400

    added sentence summarizing ADAR family expression patterns

--- RNA_localization_and_age/ADAR_expression.R
+++ RNA_localization_and_age/ADAR_expression.R
@@ -27,3 +27,10 @@ ADAR = rbind(cbind(as.data.frame(Apres[rownames(adars),1:6]),
                    Comparison = "By Age in Nucleus"))
 
 write.csv(ADAR, file="./Dropbox/sorted_figures/new/github_controlled/RNA_localization_and_age/data/ADAR_gene_expression.csv", quote = F, row.names = F)
+
+ADAR[which(ADAR$padj<=0.05),]
+# ADAR is not DE by fraction or age, 
+# but ADARB2, ADARB2-AS1, ADARB1 are greater in adult than prenatal in cytosol, 
+# and ADARB2 and ADARB1 are greater in adult than prenatal in nucleus.
+
+
